9766adcdc4ca2e25ae3b432ed63d0ddb86872ef0
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-10-19 13:38:03 +0200

    port service

--- src/internal/core/ports/services.go
+++ src/internal/core/ports/services.go
@@ -1,5 +1,8 @@
 package ports
 
+import "github.com/D-D-EINA-Calendar/CalendarServer/src/internal/core/domain"
+
 type HorarioService interface {
+	GetAvaiableHours(terna domain.Terna) ([]domain.AvaiableHours, error)
 	//TODO funciones de este service
 }
