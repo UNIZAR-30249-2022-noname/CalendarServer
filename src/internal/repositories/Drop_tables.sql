4e6fd6449d43516e3f1bbd6abd6b8b012b6da1dd
dolansete <dolancc00@gmail.com>
2021-10-31 19:24:47 +0100

    Create tables y drop tables

---
+++ src/internal/repositories/Drop_tables.sql
@@ -0,0 +1,8 @@
+DROP TABLE app_db.perteneceA;
+DROP TABLE app_db.entradahorario;
+DROP TABLE app_db.hora;
+DROP TABLE app_db.asignatura;
+DROP TABLE app_db.titulacion;
+DROP TABLE app_db.grupodocente;
+DROP TABLE app_db.curso;
+DROP TABLE app_db.aula;
