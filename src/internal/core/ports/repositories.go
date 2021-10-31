03ccb741e30e9071d204db6514b64c8f42cef465
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-10-31 20:17:52 +0100

    another TODO

--- src/internal/core/ports/repositories.go
+++ src/internal/core/ports/repositories.go
@@ -3,7 +3,6 @@ package ports
 import "github.com/D-D-EINA-Calendar/CalendarServer/src/internal/core/domain"
 
 type HorarioRepositorio interface {
-	//TODO funciones de este repo
 	GetAvailableHours(domain.Terna) ([]domain.AvailableHours, error)
 	SaveEntry(domain.Entry) error
 }
