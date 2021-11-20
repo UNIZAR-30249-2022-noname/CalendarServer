989c48cfa58e4ed7935cf598413ad22dae263430
dolansete <dolancc00@gmail.com>
2021-11-20 17:32:40 +0100

    Borrar

--- src/internal/repositories/horarioRepositorio_test.go
+++ src/internal/repositories/horarioRepositorio_test.go
@@ -10,10 +10,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+
 func TestBasico(t *testing.T) {
+	
+	//Prepare
 	//err := apperrors.ErrSql
 	assert := assert.New(t)
-	//Prepare
 	hoursexpected := []domain.AvailableHours{
 		{
 			Subject:   domain.Subject{Kind: 1,Name: "Proyecto Software"},
@@ -33,42 +35,40 @@ func TestBasico(t *testing.T) {
 	}
 
 	repos := horarioRepositorio.New()
-	repos.RawExec(consultas.Titulacion1)
-	repos.RawExec(consultas.Titulacion2)
-	repos.RawExec(consultas.Curso1)
-	repos.RawExec(consultas.Curso2)
-	repos.RawExec(consultas.Asignatura1)
-	repos.RawExec(consultas.Asignatura2)
-	repos.RawExec(consultas.Grupodocente1)
-	repos.RawExec(consultas.Grupodocente2)
-	repos.RawExec(consultas.Hora1)
-	repos.RawExec(consultas.Hora2)
+	repos.RawExec(consultas.Titulacion1); 	repos.RawExec(consultas.Titulacion2)
+	repos.RawExec(consultas.Curso1); 		repos.RawExec(consultas.Curso2)
+	repos.RawExec(consultas.Asignatura1); 	repos.RawExec(consultas.Asignatura2)
+	repos.RawExec(consultas.Grupodocente1); repos.RawExec(consultas.Grupodocente2)
+	repos.RawExec(consultas.Hora1); 		repos.RawExec(consultas.Hora2)
 
 	//Start
 	hoursgot, _ := repos.GetAvailableHours(ternaAsked)
-	/*
-	if error != nil {
-		assert.Equal(t, err, error)
-	}
-	*/
+	//if error != nil { assert.Equal(t, err, error)}
 	assert.Equal(len(hoursgot), len(hoursexpected), "Should be the same length")
 	for i, h := range hoursgot {
 		assert.Equal(h, hoursexpected[i], "Should be the same AvaiableHours")
 	}
+
+	//Delete
+	repos.RawExec(consultas.TruncHora); 		repos.RawExec(consultas.TruncGrupo)
+	repos.RawExec(consultas.TruncAsignatura); 	repos.RawExec(consultas.TruncCurso)
+	repos.RawExec(consultas.TruncTitulacion)
 	repos.CloseConn()
 }
 
+//TODO crear una funcion search entry para ver si se ha creado o deleteado
 func TestCreateEntry(t *testing.T) {
 	err := apperrors.ErrSql
 	entryAsked := domain.Entry{
 		Init: domain.NewHour(1,30),
 		End: domain.NewHour(2,40),
-		Subject: domain.Subject{Kind: 1, Name: "si"},
+		Subject: domain.Subject{Kind: 1, Name: "Proyecto Software"},
 		Room: domain.Room{Name: "1"},
 		Week: "",
 		Group: "",
 	}
 	repos := horarioRepositorio.New()
+	repos.RawExec(consultas.Aula1);
 	error := repos.CreateNewEntry(entryAsked)
 	if error != nil {
 		assert.Equal(t, err, error)
